105fcaca7029e50cc33016f9718230bd91e735bf
DavidSarumeno <56524973+DavidSarumeno@users.noreply.github.com>
2021-06-11 09:45:02 -0500

    Add files via upload

---
+++ src/ec/edu/ups/entidad/Cliente.java
@@ -0,0 +1,114 @@
+package ec.edu.ups.entidad;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+/**
+ * Entity implementation class for Entity: Cliente
+ *
+ */
+
+@Entity 
+
+public class Cliente implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+	private int Id;
+	private String nombre;
+	private String cedula;
+	private String direccion;
+	private String telefono;
+
+	
+	public Cliente(int id, String nombre, String cedula, String direccion, String telefono) {
+		super();
+		Id = id;
+		this.nombre = nombre;
+		this.cedula = cedula;
+		this.direccion = direccion;
+		this.telefono = telefono;
+	}
+	public Cliente() {
+		super();
+	}
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Id;
+		result = prime * result + ((cedula == null) ? 0 : cedula.hashCode());
+		result = prime * result + ((direccion == null) ? 0 : direccion.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result + ((telefono == null) ? 0 : telefono.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (Id != other.Id)
+			return false;
+		if (cedula == null) {
+			if (other.cedula != null)
+				return false;
+		} else if (!cedula.equals(other.cedula))
+			return false;
+		if (direccion == null) {
+			if (other.direccion != null)
+				return false;
+		} else if (!direccion.equals(other.direccion))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (telefono == null) {
+			if (other.telefono != null)
+				return false;
+		} else if (!telefono.equals(other.telefono))
+			return false;
+		return true;
+	}
+	
+	
+	
+   
+}
